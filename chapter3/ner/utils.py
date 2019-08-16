b08bdf94d99cb6e22744095f2251b49aa5adfde5
michaelliu <841412988@qq.com>
2019-08-16 15:16:53 +0800

    modify

--- chapter3/ner/utils.py
+++ chapter3/ner/utils.py
@@ -96,7 +96,7 @@ def get_ORG_entity(tag_seq,char_seq):
 def get_logger(filename):
     logger = logging.getLogger('logger')
     logger.setLevel(logging.DEBUG)
-    logging.basicConfig(format='%(message)',level=logging.DEBUG)
+    logging.basicConfig(format='%(message)s',level=logging.DEBUG)
     handler = logging.FileHandler(filename)
     handler.setLevel(logging.DEBUG)
     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(message)s'))
