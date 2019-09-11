1fbcf8693b649f1d9cc0ea8f2b4c1095758ec784
michaelliu <841412988@qq.com>
2019-09-11 13:02:32 +0800

    update

--- chapter5/FastText/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-#-*-coding:utf-8-*-
-
-import sys,os
-
-sys.path.append(u'D:\\liuyu\\桌面\\git\\BotInAction\\chapter5\\FastText\\')
---
+++ chapter5/fasttext/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
+import sys,os
+
+sys.path.append(u'D:\\liuyu\\桌面\\git\\BotInAction\\chapter5\\fasttext\\')
