925402a1698b5aa77746ab15e520cbd238ba0ca5
michael.liu <841412988@qq.com>
2019-09-19 19:08:03 +0800

    update

--- chapter10/unsupervised/main.py
+++ chapter10/unsupervised/main.py
@@ -5,26 +5,18 @@ import json
 from tfidf import *
 
 corpus_filepath = u"../../data/chapter10/AnyQ_dataset_new_2.json"
-
+stop_words_filepath = u"stop_words.utf8"
 
 def bot_process(inputfilepath):
+    qaList = []
     with open(inputfilepath, 'r', encoding='utf-8')as fp:
         content = fp.readlines()
-        print(type(content))
-        #s = json.dumps(content, ensure_ascii=False)
-        #dict = json.loads(s)
-        #print(content)
-        #print(s)
-        # content = json.load(fp)
-        #content = fp.readlines()
-        #dict = json.dumps(content)
-        #dict = json.loads(content)
-        #print(content)
-    #tfmodel = TfidfModel(dict,None,None,0)
-    #tfmodel.get_top_n_answer("上飞机可以带充电宝吗", n=15, interval=0.2, answer_num=5)
-
+        for line in content:
+            dict = json.loads(line)
+            qaList.append(dict)
+    #print(qaList)
+    tfmodel = TfidfModel(qaList, stop_words_filepath, None, 0)
 
 
 if __name__ == '__main__':
-   #load_data(corpus_filepath)
    bot_process(corpus_filepath)
